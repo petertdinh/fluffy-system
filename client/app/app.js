a61cd41d82168d02765207c036d43e37e0801d9b
Peter <peter.dinh@gmail.com>
2016-05-20 18:52:36 -0700

    [progress] implement challengeController and challengeView

--- client/app/app.js
+++ client/app/app.js
@@ -1,6 +1,13 @@
 'use strict';
-angular.module('app', ['ui-router'])
-
+angular.module('app', [
+    'app.challenge',
+    'ui-router'
+    ])
 .config(function($stateProvider, $urlRouterProvider, $httpProvider){
-    
+    $stateProvider
+        .state('challenge', {
+            url: '/challenge',
+            templateUrl: 'components/challenge/challengeView.html',
+            controller: 'challengeController',
+        })
 })
---
+++ client/app/components/challenge/challengeController.js
@@ -0,0 +1,5 @@
+'use strict';
+angular.module('app.challenge', [])
+    .controller('challengeController', ['$scope', function($scope) {
+        
+    }])
