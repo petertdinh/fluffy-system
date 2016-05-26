bf5c20fa83e01e647dacad5c56c3317a3464a055
Peter <peter.dinh@gmail.com>
2016-05-26 15:59:13 -0700

    [feature] implement data flow for home feed

--- client/app/components/home/homeController.js
+++ client/app/components/home/homeController.js
@@ -1,6 +1,14 @@
 'use strict';
 angular.module('app.home', [])
-    .controller('homeController', function($scope) {
-        $scope.feeds = [];
+    .controller('homeController', function($scope, $http) {
+    	$scope.reverse = true;
+    	function getPosts(){
+	        $http.get('/home')
+	        .then(function(resp){
+	        	$scope.feeds = resp.data;
+	        })
+	    }
+	    //calling getPosts on page load.
+	    getPosts();
     });
 
--- server/router.js
+++ server/router.js
@@ -3,21 +3,41 @@ var recipes = require('./recipesAPI.js');
 var multer = require('multer');
 var fs = require('fs');
 var posts = require("./models/post.js");
+var moment = require('moment')
 
-var upload = multer({ dest: 'server/uploads/',
+var upload = multer({ dest: 'uploads/',
     filename: function(req, file, cb) {
         cb(null, file.fieldname + '-' + Date.now())
     }
 });
 
 module.exports = function(app){
+
 app.get('/add', function(req, res){
     res.status(200).send(recipes.meals);
 });
 
+
+app.get('/home', function(req, res){
+  posts.Post.find(function (err, posts) {
+   if (err) return console.error(err);
+    var posts = posts.map(function(post){
+      var time = moment(post.timeStamp).fromNow();
+      var copy = { 
+        timeStamp: time,
+        mealPhoto: post.mealPhoto,
+        mealName: post.mealName,
+        mealNotes: post.mealNotes
+      };
+      return copy;
+    });
+    res.status(200).send(posts);
+  });
+});
+
 app.post('/upload', upload.fields([{name: 'image'}, {name: 'text'}, {name: 'title'}]), function(req, res) {
     //console.log(req);
-    res.redirect('/');
+    res.redirect('/#/home');
     var user1 = new posts.Post({ timeStamp: Date.now(),
       mealPhoto:  req.files['image'][0].filename,
       mealName: req.body.title,
@@ -32,7 +52,7 @@ app.post('/upload', upload.fields([{name: 'image'}, {name: 'text'}, {name: 'titl
 
 app.get('/uploads/*', function(req, res) {
     res.writeHead(200, {'Content-Type': 'image/png'});
-    fs.readFile(__dirname+ req.url, function(err, data) {
+    fs.readFile(__dirname + req.url, function(err, data) {
         if(err) throw err;
         res.end(data);
     });
